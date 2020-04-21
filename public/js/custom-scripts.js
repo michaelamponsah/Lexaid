7ac53a5f0156c4b8964ccf69688edd3444f8ffee
codelink223 <amponsahmichaelkwame@gmail.com>
2020-04-21 04:01:17 +0100

    Sections of the admin dashboard updated

--- public/js/custom-scripts.js
+++ public/js/custom-scripts.js
@@ -9,4 +9,73 @@
             }, 800);
         }
     })
+
+
+
+
+
+
+
+// TINYMCE TEXT EDITOR CONFIGURATIONS
+
+tinymce.init({
+    selector: "#postBody",
+    height: 600,
+    plugins: [
+  'advlist autolink link code image lists charmap print preview hr anchor pagebreak spellchecker',
+  'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+  'save table directionality emoticons template paste'
+   ],
+   toolbar: 'insertfile undo redo | image code | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons',
+   images_upload_url: 'upload.php',
+       // override default upload handler to simulate successful upload
+    images_upload_handler: function (blobInfo, success, failure) {
+        var xhr, formData;
+      
+        xhr = new XMLHttpRequest();
+        xhr.withCredentials = false;
+        xhr.open('POST', 'upload.php');
+      
+        xhr.onload = function() {
+            var json;
+        
+            if (xhr.status != 200) {
+                failure('HTTP Error: ' + xhr.status);
+                return;
+            }
+        
+            json = JSON.parse(xhr.responseText);
+        
+            if (!json || typeof json.location != 'string') {
+                failure('Invalid JSON: ' + xhr.responseText);
+                return;
+            }
+        
+            success(json.location);
+        };
+      
+        formData = new FormData();
+        formData.append('file', blobInfo.blob(), blobInfo.filename());
+      
+        xhr.send(formData);
+    },
+  
+  });
+  
+  
+
+
+
+
+
+
+
+
+
 });
+
+
+
+
+
+
